e1bbe1a84177d5f01c39890e32d343de5b599135
Bishal Roy <bishalroy895@gmail.com>
2024-05-09 22:36:44 +0530

    feat: create struct and create function for test servers

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http/httputil"
+	"net/url"
+)
+
+type Server struct {
+	address string
+	proxy   *httputil.ReverseProxy
+}
+
+func createServer(address string) Server {
+	targetUrl, err := url.Parse(address)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reverseProxy := httputil.NewSingleHostReverseProxy(targetUrl)
+
+	return Server{
+		address: address,
+		proxy:   reverseProxy,
+	}
+}
 
 func main() {
 	fmt.Println("Hello world")
+
+	servers := []Server{
+		createServer("http://localhost:3001"),
+		createServer("http://localhost:3002"),
+		createServer("http://localhost:3003"),
+	}
+
+	for _, server := range servers {
+		fmt.Print(server)
+	}
 }
