2a6515990dc3febb21f4be14b23e319141523826
Bishal Roy <bishalroy895@gmail.com>
2024-05-10 21:17:06 +0530

    feat: add reverse proxy

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
@@ -10,6 +11,11 @@ import (
 type Server struct {
 	address string
 	proxy   *httputil.ReverseProxy
+}
+
+type LoadBalancer struct {
+	servers         []Server
+	roundRobinCount int
 }
 
 func createServer(address string) Server {
@@ -27,6 +33,13 @@ func createServer(address string) Server {
 	}
 }
 
+func createLoadBalancer(servers []Server) LoadBalancer {
+	return LoadBalancer{
+		servers:         servers,
+		roundRobinCount: 0,
+	}
+}
+
 func main() {
 	fmt.Println("Hello world")
 
@@ -36,7 +49,24 @@ func main() {
 		createServer("http://localhost:3003"),
 	}
 
-	for _, server := range servers {
-		fmt.Print(server)
+	lb := createLoadBalancer(servers)
+
+	for _, server := range lb.servers {
+		fmt.Println(server.address)
+	}
+
+	// for _, server := range servers {
+	// 	fmt.Print(server)
+	// }
+
+	http.HandleFunc("/", reverseProxyHandler(lb.servers[1].proxy))
+
+	http.ListenAndServe(":8000", nil)
+}
+
+func reverseProxyHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	fmt.Printf("Forwarding request to: ")
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
 	}
 }
